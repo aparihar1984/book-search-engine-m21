b765ce4cc4374b4f47f79e8479c6bc9981efd58e
ashok parihar <aparihar@email.arizona.edu>
2024-05-26 23:21:13 -0600

    Creating an Apollo Provider to make sure every request works.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,55 @@
+import React from "react";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+
 import './App.css';
-import { Outlet } from 'react-router-dom';
+// import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    /* Provider wraps all the logic that handles/updates our state */
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+      {/* <Outlet /> */}
+    </ApolloProvider>
   );
 }
 
